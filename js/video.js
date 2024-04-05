b5d5bdbacc000b7dcdad833c09f9fa14e35fcc7d
Jane Joyce <156968287+janejoycee@users.noreply.github.com>
2024-04-04 21:15:50 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -16,7 +16,10 @@ var play_button = document.querySelector('#play');
 play_button.addEventListener("click", function() {
 	video.play();
 	console.log("Play Video")
-	// what does it mean by update the volume information
+	video.volume
+	console.log("Current volume: " + (video.volume * 100) + "%");
+
+	// Video volume [ after ] play is not displaying correctly. Do you have the correct number? Did you remember the % sign?
 });
 
 var pause_button = document.querySelector('#pause');
@@ -27,6 +30,7 @@ pause_button.addEventListener("click", function() {
 	console.log("Video is paused (true or false): " + video.paused)
 
 });
+// paused function works
 
 var slow_button = document.querySelector("#slower");
 var video = document.querySelector('.video');
@@ -40,6 +44,7 @@ function slow_down() {
 };
 
 slow_button.addEventListener("click", slow_down);
+// slow down function works
 
 var speed_button = document.querySelector('#faster');
 
@@ -57,8 +62,45 @@ function speed_up() {
 };
 
 speed_button.addEventListener("click", speed_up);
+// this is not speeding up the correct amount
 
 var skip_button = document.querySelector("#skip")
+var current_time = video.currentTime // current playback time in seconds
+
+function advance_time() {
+    if (video.ended) {
+        video.currentTime = 10; 
+        console.log('Restarted the video');
+		console.log("Current time is " + Math.round(video.currentTime) + " seconds");
+
+    } else {
+        video.currentTime += 10; 
+        console.log('Advanced playback time by 10 seconds')
+		console.log("Current time is " + Math.round(video.currentTime) + " seconds");
+    }
+};
+
+skip_button.addEventListener("click", advance_time);
+
+var mute_button = document.querySelector("#mute");
+function get_muted() {
+    var current_vol = video.volume;
+    if (mute_button.innerHTML == "Mute") {
+        video.volume = 0.0;
+        mute_button.innerHTML = "Unmute"; 
+        console.log("The current volume is " + video.volume);
+    } else {
+        video.volume = current_vol;
+        mute_button.innerHTML = "Mute"; 
+        console.log("The current volume is " + video.volume);
+    }
+}
+
+mute_button.addEventListener("click", get_muted);
+
+var slider = document.querySelector("#slider");
+
+
 
 
 
